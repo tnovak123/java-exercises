48003014b06121084d21759abc1f0abca0cffe7a
tnovak123 <timothy.m.novak@gmail.com>
2018-05-07 18:24:21 -0500

    Added Do...while loop.

--- src/exercises/Area.java
+++ src/exercises/Area.java
@@ -6,17 +6,22 @@ public class Area {
     public static void main(String[] args) {
         double r;
         double A = 0.00;
+        boolean again = true;
         Scanner in;
         final double pi = 3.14;
 
-        in = new Scanner(System.in);
-        System.out.println("Enter a number.");
-        if (in.hasNextDouble()) {
-            r = in.nextDouble();
-            A = pi * r * r;
-        } else
-            System.out.println("Enter a valid floating point number!");
+        do {
+            in = new Scanner(System.in);
+            System.out.println("Enter a number.");
+            if (in.hasNextDouble()) {
+                r = in.nextDouble();
+                A = pi * r * r;
+            } else
+                System.out.println("Enter a valid floating point number!");
 
-        System.out.println("The Area of the circle = " + A);
+            System.out.println("The Area of the circle = " + A);
+            System.out.println("Would you like to enter a new number? (true or false)");
+            again = in.nextBoolean();
+        }while(again == true);
     }
 }

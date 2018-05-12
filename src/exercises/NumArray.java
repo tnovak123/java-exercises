3fe8e5222880c2fb9453efadd76023e2b976b14c
tnovak123 <timothy.m.novak@gmail.com>
2018-05-12 08:38:26 -0500

    used integer array

---
+++ src/exercises/NumArray.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class NumArray {
+    public static void main(String[] args) {
+
+        Integer[] intlist = {1, 1, 2, 3, 5, 8};
+
+        for (int i = 0; i < intlist.length; i++) {
+            System.out.println(intlist[i]);
+        }
+    }
+}
